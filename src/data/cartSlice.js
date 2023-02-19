f925f646f7b596c03dcd3e04ef73c640cd6dc5cd
anon-pradip <pradipsubedi360@gmail.com>
2023-02-19 09:09:52 +0545

    initial commit

---
+++ src/data/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cartProductIds: []
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.cartProductIds = [...state.cartProductIds, action.payload]
+    },
+    removeFromCart: (state, action) => {
+      const indexOfId = state.cartProductIds.findIndex(action.payload)
+      state.cartProductIds.splice(indexOfId, 1)
+    },
+    clearAllItems: (state) => {
+      state.cartProductIds = []
+    }
+  }
+})
+
+export default cartSlice
